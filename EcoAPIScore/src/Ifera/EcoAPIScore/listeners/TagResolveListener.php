a276d61a7922e812f1fadbef9d021678ea4d89f4
Keenan Yafiq <accbruh0@gmail.com>
2022-01-22 00:03:36 +0700

    EcoAPIScore support API4: Expand & Fix TagResolveListener (1)

--- EcoAPIScore/src/Ifera/EcoAPIScore/listeners/TagResolveListener.php
+++ EcoAPIScore/src/Ifera/EcoAPIScore/listeners/TagResolveListener.php
@@ -11,16 +11,19 @@ use function count;
 use function explode;
 use function strval;
 
-class TagResolveListener implements Listener{
+class TagResolveListener implements Listener
+{
 
 	/** @var Main */
 	private $plugin;
 
-	public function __construct(Main $plugin){
+	public function __construct(Main $plugin)
+        {
 		$this->plugin = $plugin;
 	}
 
-	public function onTagResolve(TagsResolveEvent $event){
+	public function onTagResolve(TagsResolveEvent $event): void
+        {
 		$tag = $event->getTag();
 		$tags = explode('.', $tag->getName(), 2);
 		$value = "";
